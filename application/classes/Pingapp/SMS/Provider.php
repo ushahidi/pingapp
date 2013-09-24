074e283be6b3a8e5b6937ebfdb6398687833206d
Emmanuel Kala <emkala@gmail.com>
2013-09-24 19:52:18 +0300

    Wiring for SMS provider implementations
    * Added base class for all SMS providers
    
    * Added template SMS config file

---
+++ application/classes/Pingapp/SMS/Provider.php
@@ -0,0 +1,72 @@
+<?php defined('SYSPATH') or die('No direct access allowed'); 
+
+/**
+ * Base class for all SMS providers
+ *
+ * @author     Ushahidi Dev Team, Emmanuel Kala <emkala(at)gmail.com>
+ * @package    PingApp - http://ping.ushahidi.com
+ * @copyright  Ushahidi - http://www.ushahidi.com
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License Version 3 (GPLv3)
+ *
+ */
+abstract class Pingapp_SMS_Provider {
+	
+	/**
+	 * Authentication parameters for the default SMS provider
+	 * @var array
+	 */
+	protected $auth_params = array();
+	
+	protected static $_instance = NULL;
+	
+	public static function instance()
+	{
+		if (isset(self::$_instance))
+		{
+			return $_instance;
+		}
+		// Load configuration params
+		$config = Kohana::$config->load('sms')->as_array();
+
+		$provider_name = ucfirst(strtolower($config['provider']));
+		
+		$class_name = 'Pingapp_SMS_Provider_'.$provider_name;
+		
+		if ( ! class_exists($class_name))
+		{
+			throw new Pingapp_Exception(__("Implementation for ':provider' SMS provider not found",
+			    array(":provider" => $provider_name)));
+		}
+		
+		// Create an instance of the SMS provider
+		self::$_instance = new $class_name;
+		
+		// Check if the provider is a subclass of Pingapp_SMS_Provider
+		if ( ! is_a(self::$_instance, 'Pingapp_SMS_Provider'))
+		{
+			throw new Pingapp_Exception(__("':class' must extend the Pingapp_SMS_Provider class",
+				array(":provider" => $class_name)));
+		}
+		
+		self::$_instance->set_auth_params($config['auth_params']);
+		
+		return self::$_instance;
+	}
+	
+	/**
+	 * Sets the authentication parameters for the SMS provider
+	 *
+	 * @param  array auth_params
+	 */
+	public function set_auth_params($auth_params)
+	{
+		$this->auth_params = $auth_params;
+	}
+	
+	/**
+	 * @param  string  from  Phone number sending the message
+	 * @param  string  to    Phone number to receive the message
+	 * @param  string  message Message to be sent
+	 */
+	abstract public function send($from, $to, $message);
+}
