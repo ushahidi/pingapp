849f073549ab9d070a3a2689e5e47b21c0e69cc7
David Kobia <david@ushahidi.com>
2013-09-25 12:05:00 -0400

    Prevent secondary contacts from having secondary contacts

--- application/views/pages/person/view.php
+++ application/views/pages/person/view.php
@@ -1,7 +1,10 @@
 <h3><?php echo $person->first_name.' '.$person->last_name; ?></h3>
 <a href="/person/edit/<?php echo $person->id; ?>" class="small button" id="ping-add-contact">Edit</a>
+<?php
+// Make sure this isn't a secondary contact
+if ( $person->parent_id == 0 ): ?>
 <a href="/person/edit/?parent_id=<?php echo $person->id; ?>" class="small button" id="ping-add-contact">Add Secondary Contact</a>
-
+<?php endif;?>
 <div class="panel">
 	Status: <strong><?php echo strtoupper($person->status); ?></strong> (<?php echo date('Y-m-d', strtotime($person->updated)); ?>)
 </div>
