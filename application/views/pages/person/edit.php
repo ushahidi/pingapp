8fa5ac6ccdc78999318b686da9f90a695cdbf30c
middle8media <seth@middle8media.com>
2013-09-25 12:44:51 -0400

    New Person UI
    Restyled the UI for add "New Person"

--- application/views/pages/person/edit.php
+++ application/views/pages/person/edit.php
@@ -23,31 +23,26 @@
 		<legend>Name</legend>
 		<div class="new-name-row">
 			<div class="new-name-first">
-				<label>First Name</label>
-				<?php echo Form::input("first_name", $post['first_name'], array("id" =>"first_name", "required" => "required")); ?>
+				<?php echo Form::input("first_name", $post['first_name'], array("id" =>"first_name", "placeholder" => "First Name", "required" => "required")); ?>
 			</div>
 			<div class="new-name-last">
-				<label>Last Name</label>
-				<?php echo Form::input("last_name", $post['last_name'], array("id" =>"last_name", "required" => "required")); ?>
+				<?php echo Form::input("last_name", $post['last_name'], array("id" =>"last_name", "placeholder" => "Last Name", "required" => "required")); ?>
 			</div>
 		</div>
 	</fieldset>
 
 	<fieldset>
 		<legend>Contact Information</legend>
-		<div class="panel" id="contact[0]">
+		<div id="contact[0]">
 			<div class="contact-info-row">
 					<div class="contact-info-type">
-						<label>Type</label>
 						<?php echo Form::select("contact[0][type]", Pingapp_Form::contact_types(), (isset($post['contact'][0]['type'])) ? $post['contact'][0]['type'] : '', array("id" => "contact[0][type]", "minlength" => "3", "class" => "medium")); ?>				
 					</div>
 
 					<div class="contact-info-account">
-						<label>Account</label>
-						<?php echo Form::input("contact[0][contact]", (isset($post['contact'][0]['contact'])) ? $post['contact'][0]['contact'] : '', array("id" => "contact[0][contact]", "minlength" => "3")); ?>
+						<?php echo Form::input("contact[0][contact]", (isset($post['contact'][0]['contact'])) ? $post['contact'][0]['contact'] : '', array("id" => "contact[0][contact]", "placeholder" => "Account", "minlength" => "3")); ?>
 					</div>
 					<div class="large-4 columns">
-						<label>&nbsp;</label>
 						<a class="small button secondary ping-del-contact">Remove</a>
 					</div>
 			</div>
@@ -61,15 +56,12 @@
 		<div class="panel" id="contact[<?php echo $key; ?>]">
 			<div class="contact-info-row">
 					<div class="contact-info-type">
-						<label>Type</label>
 						<?php echo Form::select("contact[".$key."][type]", Pingapp_Form::contact_types(), (isset($post['contact'][$key]['type'])) ? $post['contact'][$key]['type'] : '', array("id" => "contact[".$key."][type]", "minlength" => "3", "class" => "medium")); ?>				
 					</div>
 					<div class="contact-info-account">
-						<label>Account</label>
-						<?php echo Form::input("contact[".$key."][contact]", (isset($post['contact'][$key]['contact'])) ? $post['contact'][$key]['contact'] : '', array("id" => "contact[".$key."][contact]", "minlength" => "3")); ?>			
+						<?php echo Form::input("contact[".$key."][contact]", (isset($post['contact'][$key]['contact'])) ? $post['contact'][$key]['contact'] : '', array("id" => "contact[".$key."][contact]", "placeholder" => "Account", "minlength" => "3")); ?>			
 					</div>
-					<div class="large-4 columns">
-						<label>&nbsp;</label>
+					<div class="remove-contact">
 						<a class="small button secondary ping-del-contact">Remove</a>
 					</div>
 			</div>
@@ -78,7 +70,7 @@
 		endforeach;
 		endif;
 		?>
-		<a class="small button secondary" id="ping-add-contact">Add Another Contact [+]</a>
+		<a class="add-contact small button secondary" id="ping-add-contact">Add Another Contact [+]</a>
 	</fieldset>
 
 	<div class="add-new-person-submit">
